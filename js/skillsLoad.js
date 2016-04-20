6de7e165f6089279a143559235c326dc397ef3d6
Playmaker8 <playmaker8@ukr.net>
2016-04-20 18:49:07 +0300

    Image grid done

--- js/skillsLoad.js
+++ js/skillsLoad.js
@@ -5,7 +5,7 @@ window.onscroll = function () {
 }
 	
 function elast(){
-		if (document.body.scrollTop > 2300 || document.documentElement.scrollTop > 2300){
+		if (document.body.scrollTop > 2200 || document.documentElement.scrollTop > 2200){
 	 document.getElementById('liquid2').style.animation = "elastic2 3s ease-in-out";
 	 document.getElementById('liquid1').style.animation = "elastic1 3s ease-in-out";
 	 document.getElementById('liquid3').style.animation = "elastic3 3s ease-in-out";
@@ -14,7 +14,7 @@ function elast(){
 var width = 1;
 var id = setInterval(func, 25);
 function func(){
-	if (document.body.scrollTop > 2300 || document.documentElement.scrollTop > 2300){
+	if (document.body.scrollTop > 2200 || document.documentElement.scrollTop > 2200){
 	if(width < 95){
 		width++;
 		document.getElementById('liquid1').innerHTML = width*1 + "%";
